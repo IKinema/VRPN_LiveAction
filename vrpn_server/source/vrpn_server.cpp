decca7c29217ddc436c067b19ce9ff855c0ac797
Hristo Petrov <h.petrov@ikinema.com>
2017-05-10 10:26:30 +0100

    Added license notice to source files

--- vrpn_server/source/vrpn_server.cpp
+++ vrpn_server/source/vrpn_server.cpp
@@ -1,3 +1,10 @@
+// Copyright © 2017, IKinema Ltd. All rights reserved.
+// IKinema VRPN SDK, source distribution
+// Your use and or redistribution of this software in source and / or binary form, with or without modification, is subject to: 
+// 1. your ongoing acceptance of and compliance with the terms and conditions of the IKinema License Agreement; and 
+// 2. your inclusion of this notice in any version of this software that you use  or redistribute.
+// A copy of the IKinema License Agreement is available by contacting IKinema Ltd., https://www.ikinema.com, support@ikinema.com
+
 #include "vrpn_server.hpp"
 #include "json.h"
 
